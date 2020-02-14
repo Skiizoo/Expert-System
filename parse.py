09c434289c4e193ad24a05eda16fd13d745b661e
BROGGI <broggi.thomas88@gmail.com>
2020-02-14 15:25:46 +0100

    Message d'erreur + amélioration regex

--- parse.py
+++ parse.py
@@ -1,5 +1,4 @@
 import argparse
-from utils import exitWithErrors
 from step import Step
 from error import ParseError
 from rule import Rule
@@ -21,7 +20,6 @@ class Parse:
 			file = [x for x in re.sub(r'(#.*)|([\t ])', '', f.read()).split('\n') if x]
 
 			for i, line in enumerate(file):
-				print(line)
 				if current_step is None:
 					raise ParseError('stepParse == None')
 				if current_step is Step.rules:
@@ -43,50 +41,46 @@ class Parse:
 	def valid_rule(self, line, i) -> bool:
 		op = ['+', '|', '^']
 		par = ['(', ')']
-		error = "Wrong rule format at line " + str(i)
 
 		if self.valid_fact(line, i):
 			return False
 		if self.valid_query(line, i):
 			raise ParseError('Facts missing')
-
 		rules = line.split('=>')
 		if len(rules) != 2:
-			raise ParseError(error)
+			raise ParseError("Rules isn't well computed at line " + str(i))
 		for rule in rules:
 			prev = None
 			count = 0
 			for j, char in enumerate(rule):
 				if self.is_upper_alpha(char) and self.is_upper_alpha(prev):
-					raise ParseError('1')
+					raise ParseError("Two characters A-Z are following at line " + str(i))
 				elif char not in op and char not in par and not self.is_upper_alpha(char) and char != '!':
-					raise ParseError('2')
+					raise ParseError("Unknown character at line " + str(i))
 				elif char == par[0]:
 					count += 1
 					if self.is_upper_alpha(prev):
-						raise ParseError('3')
+						raise ParseError("No operator before '(' at line " + str(i))
 				elif char == par[1]:
 					if count <= 0 or (not self.is_upper_alpha(prev) and prev != par[1]):
-						raise ParseError('4')
+						raise ParseError("Closed parenthesis not open at line " + str(i))
 					count -= 1
 				elif char in op and ((not self.is_upper_alpha(prev) and not prev == par[1]) or j == len(line)):
-					raise ParseError('5')
+					raise ParseError("Incorrect character before op at line " + str(i))
 				elif char == '!' and (self.is_upper_alpha(prev) or j == len(rule)):
-					raise ParseError('6')
+					raise ParseError("Incorrect character before negation at line " + str(i))
 				prev = char
 		return True
 
 	@staticmethod
 	def valid_fact(line, i) -> bool:
-		#todo améliorer la regex -> valid if only one each [A-Z]
-		if re.match(r'=[A-Z]*', line):
+		if re.match(r'^=(?!.*?(.).*?\1)[A-Z]*$', line):
 			return True
 		return False
 
 	@staticmethod
 	def valid_query(line, i) -> bool:
-		#todo améliorer la regex -> valid if only one each [A-Z]
-		if re.match(r'\?[A-Z]*', line):
+		if re.match(r'^\?(?!.*?(.).*?\1)[A-Z]*$', line):
 			return True
 		return False
 
